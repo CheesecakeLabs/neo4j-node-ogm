5d2b2fcc0dcbd4d80ede53b9126ad6f14013e72b
Pedro Setti <pedrosettiinoue@gmail.com>
2021-05-31 11:56:14 -0300

    move runtime import to the top on index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import 'regenerator-runtime/runtime.js'
 import { Model } from './model'
 import { Field } from './field'
 import { Collection } from './collection'
 
 import { getConnection, getInstance } from './driver'
-import 'regenerator-runtime/runtime.js'
 
 export { Model, Field, getConnection, getInstance, Collection }
